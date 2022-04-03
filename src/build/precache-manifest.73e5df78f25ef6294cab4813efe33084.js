59a07463debfb9c7c5f5ca8c642bca3db56c7496
OsamaAbdelhameed <osamaabdelhameed41@gmail.com>
2022-04-03 21:35:59 +0200

    React Blog

---
+++ src/build/precache-manifest.73e5df78f25ef6294cab4813efe33084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4ade8fada44ed87571",
+    "url": "/static/css/main.de245fbf.chunk.css"
+  },
+  {
+    "revision": "4e4ade8fada44ed87571",
+    "url": "/static/js/main.4e4ade8f.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "9f79888623c052b3ab97",
+    "url": "/static/js/2.9f798886.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "c88fa136610326bd48a370c162701263",
+    "url": "/index.html"
+  }
+];
